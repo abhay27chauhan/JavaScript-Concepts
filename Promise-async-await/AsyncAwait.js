2f24629d482324b6538e2b0220ea9ff2d89164ab
Abhay <chauhan27.abhay@gmail.com>
2021-01-04 13:15:05 +0530

    added error handling in async-await

--- Promise-async-await/AsyncAwait.js
+++ Promise-async-await/AsyncAwait.js
@@ -1,12 +1,17 @@
 const myAsyncFunction = async () => {
-    const userResponse = await fetch('https://jsonplaceholder.typicode.com/users'); // returns promise
+    try{
+        const userResponse = await fetch('https://jsonplaceholder.typicode.com/users'); // returns promise
 
-    const users = await userResponse.json(); // returns promise
-    const secondUser = users[1];
-    console.log(secondUser);
+        const users = await userResponse.json(); // returns promise
+        const secondUser = users[1];
+        console.log(secondUser);
 
-    const postsResponse await fetch('https://jsonplaceholder.typicode.com/posts?userId=' + secondUser.id);
+        const postsResponse = await fetch('https://jsonplaceholder.typicode.com/posts?userId=' + secondUser.id);
 
-    const posts = await postsResponse.json();
-    console.log(posts);
+        const posts = await postsResponse.json();
+        console.log(posts);
+
+    }catch(err){
+        console.log('there was an error!!');
+    }
 }
