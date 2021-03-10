be352f01dd36638602ea63f8ac21593e913d9a9b
Abhay Chauhan <chauhan27.abhay@gmail.com>
2021-03-10 14:13:28 +0530

    activity 2

--- activity/2/mycli/commands/transfer.js
+++ activity/2/mycli/commands/transfer.js
@@ -4,7 +4,7 @@ function transferfn(fileNames, operator){
     const src = fileNames[0];
     const dest = fileNames[1];
     if(fs.existsSync(src) === false){
-        console.log(`src file named ${fname} does not exist`);
+        console.log(`src file named ${src} does not exist`);
         return;
     }
     if(operator === '>'){
--- activity/2/mycli/commands/update.js
+++ activity/2/mycli/commands/update.js
@@ -1,12 +1,49 @@
 const fs = require('fs');
 
 function updatefn(fileNames, flag){
-    if(flag === '-s'){
+    const fname = fileNames[0];
+    if(fs.existsSync(fname) === false){
+        console.log(`file named ${fname} does not exist`);
+        return;
+    }
+    if(flag == '-s'){
         
-    }else if(flag === '-n'){
+    }else if(flag == '-n'){
+        // read file
+        const dataArr = fs.readFileSync(fname, {encoding:'utf8', flag:'r'}).toString().split("\n");
 
-    }else if(flag === '-b'){
+        // delete file
+        fs.unlinkSync(fname);
 
+        // write to file -> adds numbering to even empty lines
+        for(let i=0; i<dataArr.length; i++){
+            let dataToWrite = dataArr[i].trim() + "\n"
+            if(i == dataArr.length-1){
+               dataToWrite = dataArr[i].trim()
+            }
+            fs.writeFileSync(fname, (i+1) + " " + dataToWrite, {encoding: "utf8", flag: "a+", mode: 0o666});
+        }
+        console.log(fs.readFileSync(fname, "utf8")); 
+        
+    }else if(flag == '-b'){
+        // read file
+        const dataArr = fs.readFileSync(fname, {encoding:'utf8', flag:'r'}).toString().split("\n");
+
+        // filtering out non-empty lines
+        const newDataArr = dataArr.filter(str => str.trim() !== '');
+
+        // delete file
+        fs.unlinkSync(fname);
+
+        // create and write to file
+        for(let i=0; i<newDataArr.length; i++){
+            let dataToWrite = newDataArr[i].trim() + "\n"
+            if(i == newDataArr.length-1){
+               dataToWrite = newDataArr[i].trim()
+            }
+            fs.writeFileSync(fname, (i+1) + " " + dataToWrite, {encoding: "utf8", flag: "a+", mode: 0o666});
+        }
+        console.log(fs.readFileSync(fname, "utf8"));
     }else{
         console.log('wrong command');
     }
